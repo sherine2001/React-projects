49d28a6e7e7a50838486881bb2c4dab350081845
sherine2001 <horosherine@yahoo.com>
2021-10-12 13:25:56 +0530

    React-Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+import Calculator from './Calculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Calculator></Calculator>
+    </div>
+  );
+}
+
+export default App;
